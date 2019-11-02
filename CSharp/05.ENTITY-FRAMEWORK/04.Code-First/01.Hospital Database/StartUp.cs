7610e7274d798c24663a836bad98dbbfba71c499
ulivegenov <uli_vegenov@abv.bg>
2019-11-03 00:09:50 +0200

    Some code refactoring in "Hospital Database", in "04.Code-First".

--- CSharp/05.ENTITY-FRAMEWORK/04.Code-First/01.Hospital Database/StartUp.cs
+++ CSharp/05.ENTITY-FRAMEWORK/04.Code-First/01.Hospital Database/StartUp.cs
@@ -13,18 +13,18 @@
         {
             using (var context = new HospitalContext())
             {
-                Console.WriteLine("Welcome guest! Do you have an account?");
-                Console.Write("Please write Y/N: ");
-                string answer = Console.ReadLine();
-                var commandUI = new CommandUserInterface();
-
                 try
                 {
+                    Console.WriteLine("Welcome guest! Do you have an account?");
+                    Console.Write("Please write Y/N: ");
+                    string answer = Console.ReadLine();
+                    var commandUI = new CommandUserInterface();
+
                     if (answer.ToUpper() == "Y")
                     {
                         commandUI.Login(context);
                     }
-                    else if (true)
+                    else if (answer.ToUpper() == "N")
                     {
                         commandUI.Register(context);
                     }
@@ -36,7 +36,6 @@
                 catch (ArgumentException e)
                 {
                     Console.WriteLine(e.Message);
-                    return;
                 }
             }
         }
